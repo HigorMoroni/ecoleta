fb796ebb8b45e82a89c6b63eed15aa39220f42fa
Higor M. G. Ferreira <moroni.higor@gmail.com>
2020-06-03 18:58:42 -0300

    Finalizei o back-end
    Só a imagem do ponto que ainda está estática

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -48,7 +48,7 @@ class PointsController {
         const trx = await knex.transaction();
 
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1475275083424-b4ff81625b60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
             name,
             email,
             whatsapp,
