6f1259f2a37858895e31816bd2c328605491328f
Higor M. G. Ferreira <moroni.higor@gmail.com>
2020-06-03 20:12:05 -0300

    Conceitos do React

---
+++ web/Aula/React.js
@@ -0,0 +1,4 @@
+// JSX = Sintaxe de XML (HTML) dentro do JavaScript
+// useState = Valores que serão alterados dinamicamente na página
+import React, {useState} from 'react';
+// Retorno do useState: [valor do estado, função para atualizar o valor do estado]
