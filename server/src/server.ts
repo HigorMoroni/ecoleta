4cc2d76093d64f4096a480d03f55b7d2d4c91aba
Higor M. G. Ferreira <moroni.higor@gmail.com>
2020-06-06 17:45:22 -0300

    Validando a entrada de dados e erros no backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import routes from './routes';
 import path from 'path';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -9,7 +10,9 @@ app.use(cors(
     // {origin: 'www.'} Informar Dominio da aplicação web
 ));
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
